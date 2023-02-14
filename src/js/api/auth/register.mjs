e8c68d8ada9f8d413e9dca5a7d0bf21e47595fba
Ken Thore <kenthore@live.no>
2023-02-14 13:53:56 +0100

    register function + register submit listener + some small changes to folder and filenames

---
+++ src/js/api/auth/register.mjs
@@ -0,0 +1,29 @@
+import * as constants from "../constants.mjs";
+import { registerFormListener } from "../handlers/registerFormListener.mjs";
+
+registerFormListener();
+
+const action = "/auth/register";
+const method = "POST";
+
+export async function registerUser(profile) {
+  try {
+    console.log(action);
+    const registerURL = constants.API_SOCIAL_URL + action;
+    console.log(registerURL);
+
+    const postData = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(profile),
+    };
+
+    const response = await fetch(registerURL, postData);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
