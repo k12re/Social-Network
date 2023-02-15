0fa61a8247f9e3425feb7cb5dd975eff3a58781b
Benjamel <benjamin.meldal@gmail.com>
2023-02-15 10:46:22 +0100

    some changes

--- src/js/global.mjs
+++ src/js/global.mjs
@@ -1,12 +1,9 @@
 import { setLoginFormListener } from "./api/handlers/login.mjs";
 import { registerFormListener } from "./api/handlers/registerFormListener.mjs";
 
-// setLoginFormListener();
-// registerFormListener();
-
 const path = location.pathname;
 
-if (path === "/profile/login/") {
+if (path === "/index.html") {
   setLoginFormListener();
 } else if (path === "/profile/register/") {
   registerFormListener();
