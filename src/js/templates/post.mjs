0b7d9376598b4c24eb1b7e919a22db1328c0e629
Ken Thore <kenthore@live.no>
2023-02-17 10:22:14 +0100

    made html template and preliminary clone function - MORE WORK NEEDED

--- src/js/templates/post.mjs
+++ src/js/templates/post.mjs
@@ -1,23 +1,44 @@
+//Example for createElement
+// export function postTemplate(postData) {
+//   const post = document.createElement("div");
+//   post.classList.add("title", "card", "mb-3");
+//   post.innerText = postData.title;
+
+//   if (postData.body) {
+//     const body = document.createElement("div");
+//     body.classList.add("body-text");
+//     body.innerText = postData.body;
+//     post.append(body);
+//   } else if (postData.media) {
+//     const img = document.createElement("img");
+//     img.classList.add("img-fluid", "rounded", "h-100", "object-fit-cover");
+//     img.src = postData.media;
+//     img.alt = postData.title;
+//     post.append(img);
+//   }
+
+//   return post;
+// }
+
 export function postTemplate(postData) {
-  const post = document.createElement("div");
-  post.classList.add("title", "card", "mb-3");
-  post.innerText = postData.title;
+  const template = document.querySelector("#template-post");
+  const clone = template.content.cloneNode(true);
+  clone.querySelector("h5").innerText = postData.title;
 
-  if (postData.body) {
-    const body = document.createElement("div");
-    body.classList.add("body-text");
-    body.innerText = postData.body;
-    post.append(body);
-  }
+  const bodyText = clone.querySelector(".body-text");
+  bodyText.innerText = postData.body;
 
-  return post;
+  document.body.append(clone);
+
+  // return post;
 }
 
 export function renderPostTemplate(postData, parent) {
-  parent.append(postTemplate(postData));
+  document.body.append(postTemplate(postData));
 }
 
 export function renderPostTemplates(postDataList, parent) {
   const postElements = postDataList.map(postTemplate);
-  parent.append(...postElements);
+  document.body.append(...postElements);
+  console.log(postDataList);
 }
