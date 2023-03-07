769ca87add88e53d6caca06a6474e1f4d9636fca
Benjamel <benjamin.meldal@gmail.com>
2023-03-07 20:22:20 +0100

    test

--- src/js/global.mjs
+++ src/js/global.mjs
@@ -39,7 +39,7 @@ if (path === "/index.html") {
   setUpdateProfile();
 } else if (path === "/profile/edit/index.html") {
   updateProfileMedia();
-} else if (path === "/post/comment/" || path === "/post/comment/index.html")
+} else if (path === "/post/comment/" || path === "/post/comment/index.html") {
 } else if (path === "/post/comment/" || path === "/post/comment/index.html") {
   commentPostFormListener();
 } else if (path === "/profile/" || path === "/profile/index.html") {
