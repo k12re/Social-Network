b2766a5d9f288b35f45945ec37a90a31ec961a74
Ken Thore <kenthore@live.no>
2023-03-11 23:41:26 +0100

    added jsdocs to the handlers folder

--- src/js/api/handlers/register.mjs
+++ src/js/api/handlers/register.mjs
@@ -1,15 +1,26 @@
 import { registerUser } from "../auth/register.mjs";
 
+/**
+ * Listens to the submit event on a registration form and registers a new user using the form data.
+ * @function
+ * @param {Event} event - The submit event object.
+ * @returns {void}
+ */
 export function registerFormListener() {
+  // Select the registration form
   const form = document.querySelector("#registerForm");
 
   if (form) {
     form.addEventListener("submit", (event) => {
+      // Prevent the form from submitting
       event.preventDefault();
+
+      // Get the user data from the form
       const form = event.target;
       const formData = new FormData(form);
       const profile = Object.fromEntries(formData.entries());
 
+      // Register the user using the API function
       registerUser(profile);
     });
   }
--- src/js/api/handlers/updateProfile.mjs
+++ src/js/api/handlers/updateProfile.mjs
@@ -4,33 +4,46 @@ import { headers } from "../headers.mjs";
 import { API_SOCIAL_URL } from "../constants.mjs";
 import { authFetch } from "../auth/posts/authFetch.mjs";
 
+/**
+ * Attach a submit event listener to the user profile update form to update the user's profile.
+ */
 export async function setUpdateProfile() {
+  // Get the user profile update form element
   const form = document.querySelector("#editProfile");
 
   if (form) {
+    // Load the user's profile data and pre-fill the form fields
     const { name, email } = load("profile");
     form.name.value = name;
     form.email.value = email;
 
+    // Disable the form submit button while the profile data is being fetched
     const button = form.querySelector("button");
     button.disabled = true;
 
+    // Get the user's full profile data and pre-fill the form fields for the banner and avatar
     const profile = await getProfile(name);
 
     form.banner.value = profile.banner;
     form.avatar.value = profile.avatar;
 
+    // Re-enable the form submit button
     button.disabled = false;
 
+    // Attach a submit event listener to the form
     form.addEventListener("submit", (event) => {
       event.preventDefault();
       const form = event.target;
       const formData = new FormData(form);
+
+      // Convert the form data to an object
       const profile = Object.fromEntries(formData.entries());
 
+      // Add the user's name and email to the profile object
       profile.name = name;
       profile.email = email;
 
+      // Update the user's profile
       updateProfile(profile);
     });
   }
